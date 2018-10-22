f3c261b342d52e14710545232784c09d2502e0c1
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-10-22 14:16:33 -0500

    Registering routes for microservices in Lumen from the Gateway

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,23 @@
 | It is a breeze. Simply tell Lumen the URIs it should respond to
 | and give it the Closure to call when that URI is requested.
 |
-*/
+ */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+/**
+ * Routes for authors
+ */
+$router->get('/authors', 'AuthorController@index');
+$router->post('/authors', 'AuthorController@store');
+$router->get('/authors/{author}', 'AuthorController@show');
+$router->put('/authors/{author}', 'AuthorController@update');
+$router->patch('/authors/{author}', 'AuthorController@update');
+$router->delete('/authors/{author}', 'AuthorController@destroy');
+/**
+ * Routes for books
+ */
+$router->get('/books', 'BookController@index');
+$router->post('/books', 'BookController@store');
+$router->get('/books/{book}', 'BookController@show');
+$router->put('/books/{book}', 'BookController@update');
+$router->patch('/books/{book}', 'BookController@update');
+$router->delete('/books/{book}', 'BookController@destroy');
