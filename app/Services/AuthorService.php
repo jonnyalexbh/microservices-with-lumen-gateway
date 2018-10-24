b9116f2b225639f706c919e6ea73155ccea1ae28
jonnyalexbh <jonnyalex.bh@gmail.com>
2018-10-24 10:13:32 -0500

    Editing author instances using the authors' service

--- app/Services/AuthorService.php
+++ app/Services/AuthorService.php
@@ -18,6 +18,7 @@ class AuthorService
     {
         $this->baseUri = config('services.authors.base_uri');
     }
+
     /**
      * Obtain the full list of author from the author service
      * @return string
@@ -26,6 +27,7 @@ class AuthorService
     {
         return $this->performRequest('GET', '/authors');
     }
+
     /**
      * Create one author using the author service
      * @return string
@@ -33,5 +35,23 @@ class AuthorService
     public function createAuthor($data)
     {
         return $this->performRequest('POST', '/authors', $data);
+    }
+
+    /**
+     * Obtain one single author from the author service
+     * @return string
+     */
+    public function obtainAuthor($author)
+    {
+        return $this->performRequest('GET', "/authors/{$author}");
+    }
+
+    /**
+     * Update an instance of author using the author service
+     * @return [type] [description]
+     */
+    public function editAuthor($data, $author)
+    {
+        return $this->performRequest('PUT', "/authors/{$author}", $data);
     }
 }
